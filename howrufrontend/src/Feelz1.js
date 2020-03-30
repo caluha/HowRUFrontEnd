6223edcd2106ad40554f9bd305cacbb214edaa17
HernikV <henrik.vendel@outlook.com>
2020-03-30 11:55:25 +0200

    Moore boxes

--- howrufrontend/src/Feelz1.js
+++ howrufrontend/src/Feelz1.js
@@ -5,7 +5,11 @@ class Feelz1 extends React.Component {
   render() {
     return (
       <div><h1>All Dem Feelz</h1>
-        <div class="box">Text 1</div>
+        <div class="box">Moodz</div>
+        <div class="box">Feelz</div>
+        <div class="box">Journal</div>
+        <div class="box">Create own</div>
+        <div class="box">Calender</div>
       </div>
     )
   }
