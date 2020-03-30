0366584a97601a207e6f6ebb45eaa6008ccf0a98
HernikV <henrik.vendel@outlook.com>
2020-03-30 11:38:13 +0200

    Setup react

---
+++ howrufrontend/src/Feelz1.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Feelz1 extends React.Component {
+
+  render() {
+    return (
+      <div><h1>All Dem Feelz</h1>
+        <div class="box">Text 1</div>
+      </div>
+    )
+  }
+}
+export default Feelz1
--- howrufrontend/src/index.js
+++ howrufrontend/src/index.js
@@ -2,13 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import Feelz1 from './Feelz1.js';
 
-ReactDOM.render(
-  <React.StrictMode>
-   <h1>All Dem Feelz</h1>
-   <div class="box">Text 1</div>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Feelz1/>, document.getElementById('root'));
 
 serviceWorker.unregister();
