287b20c35c45c2ab81ff7e5e01bf551ca112ca0b
HernikV <henrik.vendel@outlook.com>
2020-03-31 18:37:54 +0200

    added pens and checkboxes

--- howrufrontend/src/BaseComponent.js
+++ howrufrontend/src/BaseComponent.js
@@ -8,7 +8,10 @@ import {
     useParams
 } from "react-router-dom";
 import QuestionSet from './QuestionSet';
-import coffe from './coffe.jpg';
+import coffe from './images/coffe.jpg';
+import edit_01 from './images/edit_01.png';
+import answered_01 from './images/answered_01.png';
+import unanswered_01 from './images/unanswered_01.png';
 import Navbar from './components/Navbar';
 import {NavLink} from "react-router-dom";
 
@@ -27,18 +30,28 @@ class Base extends React.Component {
                             <Route exact path="/">
                                 <img src={coffe} style={{ width: "350px" }} />
                                 <div className="box" id="lightred_box">
+                                <NavLink exact to="/unchecked"><a className="pen"><img src={unanswered_01} style={{height:"25px"}} /></a></NavLink>
+                                <NavLink exact to="/"><a className="pen"><img src={edit_01} style={{height:"25px"}} /></a></NavLink>
                                     <NavLink exact to="/migraine">Migraine</NavLink>
                                 </div>
                                 <div className="box" id="lightred_box">
+                                <NavLink exact to="/check"><a className="pen"><img src={answered_01} style={{height:"25px"}} /></a></NavLink>
+                                <NavLink exact to="/"><a className="pen"><img src={edit_01} style={{height:"25px"}} /></a></NavLink>
                                     <NavLink exact to="/feelz">Feelz</NavLink>
                                 </div>
                                 <div className="box" id="red_box">
+                                <NavLink exact to="/check"><a className="pen"><img src={answered_01} style={{height:"25px"}} /></a></NavLink>
+                                <NavLink exact to="/"><a className="pen"><img src={edit_01} style={{height:"25px"}} /></a></NavLink>
                                     Journal
                                 </div>
                                 <div className="box" id="lightred_box">
+                                <NavLink exact to="/check"><a className="pen"><img src={answered_01} style={{height:"25px"}} /></a></NavLink>
+                                <NavLink exact to="/"><a className="pen"><img src={edit_01} style={{height:"25px"}} /></a></NavLink>
                                     Create own
                                 </div>
                                 <div className="box" id="green_box">
+                                <NavLink exact to="/unchecked"><a className="pen"><img src={unanswered_01} style={{height:"25px"}} /></a></NavLink>
+                                <NavLink exact to="/"><a className="pen"><img src={edit_01} style={{height:"25px"}} /></a></NavLink>
                                     Calender
                                 </div>
                             </Route>
