6953036d3edf469fac06bd270130d1319c45dd2f
HernikV <henrik.vendel@outlook.com>
2020-03-31 17:09:23 +0200

    Strange things happens

--- howrufrontend/src/BaseComponent.js
+++ howrufrontend/src/BaseComponent.js
@@ -22,28 +22,7 @@ class Base extends React.Component {
             <div style={{ height: "100%" }}>
                 <div className="startmenu">
                     <Router>
-<<<<<<< HEAD
-                        <Navbar />
-                        <img src={coffe} style={{ width: "375px" }} />
-                        <div className="box" id="green_box" >
-                            <Link to="/migraine">Migraine</Link>
-                            <button id="pen"> x </button>
-                        </div>
-                        <div className="box" id="red_box">
-                            <Link to="/feelz">Feelz</Link>
-                        </div>
-                        <div className="box" id="lightred_box">
-                            Journal
-                        </div>
-                        <div className="box" id="green_box">
-                            Create own
-                        </div>
-                        <div className="box" id="lightred_box">
-                            Calender
-                        </div>
-=======
                     <Navbar />   
->>>>>>> 47495ce35262f7a7cd5f217651533f4249612e61
                         <Switch>
                             <Route exact path="/">
                                 <img src={coffe} style={{ width: "auto" }} />
