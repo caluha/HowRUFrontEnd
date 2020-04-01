e2419c3063943e9d693e6ede4ceef52983568cc9
HernikV <henrik.vendel@outlook.com>
2020-04-01 17:26:47 +0200

    Now it kinda works

--- howrufrontend/src/BaseComponent.js
+++ howrufrontend/src/BaseComponent.js
@@ -9,11 +9,11 @@ import {
 } from "react-router-dom";
 import QuestionSet from './QuestionSet';
 import coffee2 from './images/coffee2.jpg';
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Navbar from './components/Navbar';
 import mockQuestionSet from './mockQuestionSet';
 import QuestionSetButton from './QuestionSetButton';
-import APIConnection from './APIConnection';
+
 
 
 class Base extends React.Component {
@@ -22,7 +22,7 @@ class Base extends React.Component {
     render() {
         return (
             <div style={{ height: "100%" }}>
-                <div className="mainpage">                   
+                <div className="mainpage">
                     <Router>
                         <Navbar />
                         <Switch>
@@ -35,10 +35,7 @@ class Base extends React.Component {
                             {routeFactory()}
                         </Switch>
                     </Router>
-                <div>
-                    <APIConnection/>
-                </div> */}
-                    
+                   
                 </div>
             </div>
 
@@ -46,12 +43,12 @@ class Base extends React.Component {
     }
 }
 
-function questionSetFactory(){
-    return mockQuestionSet.map((e) => <QuestionSetButton key={e.id} id={e.id} name={e.name}/>)                   
+function questionSetFactory() {
+    return mockQuestionSet.map((e) => <QuestionSetButton key={e.id} id={e.id} name={e.name} />)
 }
 
-function routeFactory(){
-    return mockQuestionSet.map((e) => <Route key={e.id} path={"/"+ e.name}><QuestionSet/></Route>)
+function routeFactory() {
+    return mockQuestionSet.map((e) => <Route key={e.id} path={"/" + e.name}><QuestionSet /></Route>)
 }
 
 export default Base;
