ddf604927ae832dbdf89801fcf388c08ed1a962b
caluha <ludwig.havasi@gmail.com>
2020-04-01 14:57:00 +0200

    Still doesn't work. Jacob is starting to get a little bit frustrated.

--- howrufrontend/src/Response.js
+++ howrufrontend/src/Response.js
@@ -5,9 +5,9 @@ class Response extends React.Component {
     addInput() {
         switch (this.props.type) {
             case "TEXT":
-                return <input type="text" id={this.props.id} onChange={this.props.updateAnswer}/>;
+                return <input type="text" id={this.props.id} defaultValue={this.props.defaultValue[0].value} onChange={this.props.updateAnswer}/>;
             case "RANGE":
-                return <input type="range" id={this.props.id} defaultValue={this.props.value} min={this.props.min} max={this.props.max} onChange={this.props.updateAnswer}/>
+                return <input type="range" id={this.props.id} defaultValue={this.props.defaultValue[0].value} min={this.props.min} max={this.props.max} onChange={this.props.updateAnswer}/>
             case "RADIO":
                 return <React.Fragment>
                             <input type={this.props.type} id={this.props.id} name={"select"} onChange={this.props.updateAnswer}/>
