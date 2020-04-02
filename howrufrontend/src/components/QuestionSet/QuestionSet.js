203c948da15286193f03c9cdd6827e16e297a9e0
johanback <johan.g.back@gmail.com>
2020-04-02 12:01:50 +0200

    Submit button works and sends all questionset responses to backend successfully.

--- howrufrontend/src/components/QuestionSet/QuestionSet.js
+++ howrufrontend/src/components/QuestionSet/QuestionSet.js
@@ -11,7 +11,7 @@ class QuestionSet extends React.Component {
         this.handleAnswer = this.handleAnswer.bind(this);
         this.addOne = this.addOne.bind(this);
         this.decOne = this.decOne.bind(this);
-    
+
         this.state = {
             Questions: this.props.questionSet.questions,
             questionCounter: 0,
@@ -32,19 +32,58 @@ class QuestionSet extends React.Component {
         // console.log(this.state.answers);
     }
 
-    submitData() {
-        let url = "http://localhost:8080/response"
-        let tjofräs = this.state.answers;
-
-        for (const e in this.state.answers) {
-            console.log(this.state.answers[e]);
+    submitData = () => {
+        const url = "http://localhost:8080/response";
+        for (const i in this.state.answers) {
+            for (const j in this.state.answers[i]) {
+                const data = this.state.answers[i][j];
+                console.log(data);
+                fetch(url, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(data),
+                })
+                    .then((response) => response.json())
+                    .then((data) => {
+                        console.log('Success:', data);
+                    })
+                    .catch((error) => {
+                        console.error('Error:', error);
+                    });
+            }
         }
+
+
+        // for (const i in this.state.answers) {
+        //     console.log(this.state.answers[i]);
+        //         fetch(url,{ 
+        //             method: 'post',
+        //             headers: {'Content-Type': 'application/json',
+        //           'transfer-encoding':'chunked' },
+        //             body: JSON.stringify(this.state.answers[i]) , 
+        //           })
+        // }
+        // fetch(url, {
+        //     method: 'POST',
+        //     headers: {
+        //         'Accept': 'application/json',
+        //         'Content-Type': 'application/json',
+        //     },
+        //     body: JSON.stringify({
+        //         firstParam: 'yourValue',
+        //         secondParam: 'yourOtherValue',
+        //     })
+        // })
+
+
 
         //send(tjofräs).to(TheBackend).plsWork();
     }
 
     addOne = () => {
-        if (!(this.state.questionCounter + 1 === this.state.Questions.length+1)) {
+        if (!(this.state.questionCounter + 1 === this.state.Questions.length + 1)) {
             this.setState((previousState) => ({ questionCounter: previousState.questionCounter + 1 }));
         }
     }
@@ -55,12 +94,12 @@ class QuestionSet extends React.Component {
         }
     }
 
-    storeQuestionState (questionId, questionState)  {
-        this.setState( (prev) => {
+    storeQuestionState(questionId, questionState) {
+        this.setState((prev) => {
             let ns = prev;
             ns.questionStates[questionId] = questionState;
             return ns;
-        } )
+        })
     }
 
     render() {
@@ -69,16 +108,16 @@ class QuestionSet extends React.Component {
         console.log(this.state.Questions);
         for (const e of this.state.Questions) {
 
-            
-            questionComponents.push(<Question 
-                        // initialState={this.state.answers[e.id] ? this.state.answers[e.id] : undefined }
-                        key={e.id} id={e.id} question={e.question} type={e.type}
-                        responses={e.responses} handleAnswer={this.handleAnswer}
-                        next={this.addOne} previous={this.decOne} 
 
-                        storeState={this.storeQuestionState}
-                        initialState = {this.state.questionStates[e.id] ? this.state.questionStates[e.id] : undefined }
-                        />)
+            questionComponents.push(<Question
+                // initialState={this.state.answers[e.id] ? this.state.answers[e.id] : undefined }
+                key={e.id} id={e.id} question={e.question} type={e.type}
+                responses={e.responses} handleAnswer={this.handleAnswer}
+                next={this.addOne} previous={this.decOne}
+
+                storeState={this.storeQuestionState}
+                initialState={this.state.questionStates[e.id] ? this.state.questionStates[e.id] : undefined}
+            />)
         }
         if (this.state.questionCounter === this.state.Questions.length) {
             return (
@@ -86,7 +125,7 @@ class QuestionSet extends React.Component {
                 //funktionalitet.
                 <div>
                     <div className="box_question">
-                        <button className="submitButton" onClick={this.submitData()}>SUBMIT</button>
+                        <button className="submitButton" onClick={this.submitData}>SUBMIT</button>
                     </div>
                 </div>
             )
