e96f90d33c2770d83e905034ca39a4eaa413e746
Jacob Winding <jacobkwinding@gmail.com>
2020-04-03 09:17:21 +0200

    Started styling the create-edit questionset menus.

--- howrufrontend/src/components/CreateEdit/CreateQuestionForm.js
+++ howrufrontend/src/components/CreateEdit/CreateQuestionForm.js
@@ -11,87 +11,125 @@ class CreateQuestionForm extends React.Component{
         super(props);
 
         this.updateType=this.updateType.bind(this); 
+        this.saveResponse=this.saveResponse.bind(this);
+
+        console.log(props.question);
 
         this.state = {
-            
-            question: "",
-            type: "RANGE",
-            responses: [
-                defaultResponses.RangeResponse
-            ]
+                question: (props.question ? props.question: 
+                {
+                    id: this.props.questionId,
+                    question: "",
+                    type: "RANGE",
+                    responses: [
+                        defaultResponses.RangeResponse
+                    ]
+                }),
+                errors:{
+                    question:'',
+                    responses:''
+                }
             
         }
     }
 
     updateType(event){
+        let newQuestion= this.state.question;
+        
 
-        this.setState({type : event.target.value});
+        newQuestion.type = event.target.value;
         switch(event.target.value){
             case "RANGE": 
-                this.setState({responses: [defaultResponses.RangeResponse]});
+                newQuestion.responses = [defaultResponses.RangeResponse];
             break;
             case "TEXT": 
-                this.setState({responses: [defaultResponses.TextResponse]});
+                newQuestion.responses = [defaultResponses.TextResponse];
             break;
             case "RADIO": 
-                this.setState({responses: [
+                newQuestion.responses = [
                     defaultResponses.RadioResponse1,
                     defaultResponses.RadioResponse2,
-                    defaultResponses.RadioResponse3]});
+                    defaultResponses.RadioResponse3];
             break;
             case "CHECKBOX": 
-            this.setState({responses: [
+                newQuestion.responses = [
                 defaultResponses.CheckboxResponse1,
                 defaultResponses.CheckboxResponse2,
-                defaultResponses.CheckboxResponse3]});
+                defaultResponses.CheckboxResponse3];
             break;
 
             default:
-                this.setState({
-                    type:"RANGE",
-                    responses: [defaultResponses.RangeResponse]});
+                newQuestion.type="RANGE";
+                newQuestion.responses = [defaultResponses.RangeResponse];
             
         }
-
+        this.setState({question:newQuestion});
         
         event.preventDefault(); 
     }
 
     saveResponse(responses ){
-
+        let newQuestion= this.state.question;
+        newQuestion.responses=responses;
+        this.setState({question:newQuestion});
     }
 
     updateQuestionText = (event) => {
-        this.setState({question : event.target.value})
+        let newQuestion= this.state.question;
+        newQuestion.question=event.target.value;
+        this.setState({question : newQuestion })
+
+        event.preventDefault(); 
+    }
+
+    handleSubmit = (event) => {
+        
+        this.props.saveQuestion(this.state.question)
         event.preventDefault(); 
     }
 
     render(){
-        let rangeResp = <CreateRangeResponse response = {this.state.responses[0]} />;
-        let textResp = <CreateTextResponse response = {this.state.responses[0]} />;
-        let radioCheckboxResp = <CreateRadioCheckboxResponses responses = {this.state.responses} />;
+        let rangeResp = <CreateRangeResponse response = {this.state.question.responses[0]} saveResponse={ this.saveResponse} />;
+        let textResp = <CreateTextResponse response = {this.state.question.responses[0]}  saveResponse={ this.saveResponse} />;
+        let radioCheckboxResp = <CreateRadioCheckboxResponses responses = {this.state.question.responses}  saveResponse={ this.saveResponse} />;
 
 
         return(
-            <div>
-          
-                <input type="text" onChange={this.updateQuestionText} value={this.state.question}
-                 placeholder="Question"></input>
-                
-                <label htmlFor="questionType">Type</label>
-                <select name="questionType" value={this.state.type} onChange={this.updateType}>
-                    <option value="RANGE">Range</option>
-                    <option value="TEXT">Text</option>
-                    <option value="RADIO">Radio buttons</option>
-                    <option value="CHECKBOX">Checkboxes</option>
+            <div className="questionFormModalBackground">
+                <div className="questionFormModal">
+                    <h4 className="questionFormHeadline">New Question</h4>
+                    
+                    <form onSubmit={this.handleSubmit} noValidate >
+                        <textarea className="input-group questionArea" type="textarea" onChange={this.updateQuestionText} 
+                                rows="3"
+                                value={this.state.question.question}
+                                placeholder="Question" noValidate></textarea>
+                        
+                        <div className="row">
+                            <div className="col">
+                                <label htmlFor="questionType">Type</label>
+                            </div>
+                                
+                            <div className="col">
+                                <select className="input-group" name="questionType" value={this.state.question.type} onChange={this.updateType}>
+                                    <option value="RANGE">Range</option>
+                                    <option value="TEXT">Text</option>
+                                    <option value="RADIO">Radio buttons</option>
+                                    <option value="CHECKBOX">Checkboxes</option>
+                                </select>
+                            </div>
+                        </div>
 
-                </select>
 
-                {this.state.type === "RANGE" ? rangeResp :
-                    this.state.type === "TEXT" ? textResp : 
-                    this.state.type === "RADIO" || this.state.type === "CHECKBOX"  ? radioCheckboxResp : <p>Loading</p> }
-                
-                <button onClick={() => this.props.saveQuestion(this.state.question)} className="btn btn-primary">Save</button>
+
+                        {this.state.question.type === "RANGE" ? rangeResp :
+                            this.state.question.type === "TEXT" ? textResp : 
+                            this.state.question.type === "RADIO" || this.state.question.type === "CHECKBOX"  ? radioCheckboxResp : <p>Loading</p> }
+                        
+                        <button className="btn btn-primary">Save</button>
+                    </form>
+                </div>
+            
             </div>
         )
     }
--- howrufrontend/src/components/CreateEdit/CreateRangeResponse.js
+++ howrufrontend/src/components/CreateEdit/CreateRangeResponse.js
@@ -10,7 +10,8 @@ class CreateRangeResponse extends React.Component {
     }
 
     handleFormChange(event){
-        this.setState({[event.target.id] : event.target.value })
+        this.setState({[event.target.id] : event.target.value }, () => this.props.saveResponse([this.state]));
+
         event.preventDefault(); 
     }
 
@@ -20,22 +21,47 @@ class CreateRangeResponse extends React.Component {
 
         return (
             <div>
-                <form onChange={this.handleFormChange}>
-                    <label htmlFor="min">Min</label>
-                    <input type="number" id="min" name="min" value={this.state.min} onChange={this.handleFormChange} />
-
-                    <label htmlFor="max">Max</label>
-                    <input type="number" id="max" name="max" value={this.state.max} onChange={this.handleFormChange} />
-                    
-                    <label htmlFor="mimin_descriptionn">Min description</label>
-                    <input type="text" id="min_description" name="min_description" onChange={this.handleFormChange} value={this.state.min_description} />
-
-                    <label htmlFor="max_description">Max description</label>
-                    <input type="text" id="max_description" name="max_description" onChange={this.handleFormChange} value={this.state.max_description} />
-
-                </form>
-
                 
+                <div className="row">
+                    <div className="col">
+                        <label htmlFor="min">Min</label>
+                    </div>
+                    
+                    <div className="col">
+                        <input type="number" id="min" name="min" value={this.state.min} 
+                        onChange={this.handleFormChange} />
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col">
+                        <label htmlFor="max">Max</label>
+                    </div>
+                    <div className="col">
+                        <input type="number" id="max" name="max" value={this.state.max} 
+                        onChange={this.handleFormChange} />
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col">
+                        <label htmlFor="min_descriptionn">Min description</label>
+                    </div>
+                    <div className="col">
+                        <input type="text" id="min_description" name="min_description" 
+                        onChange={this.handleFormChange} value={this.state.min_description} />
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col">
+                        <label htmlFor="max_description">Max description</label>
+                    </div>
+                    <div className="col">    
+                        <input type="text" id="max_description" name="max_description" 
+                        onChange={this.handleFormChange} value={this.state.max_description} />
+                    </div>
+                </div>
             </div>
         )
     }
