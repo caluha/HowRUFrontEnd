7b1fcd4e1cd7a3bf763cb69ee504750f2e01043c
HernikV <henrik.vendel@outlook.com>
2020-04-03 11:39:35 +0200

    graph

--- howrufrontend/src/components/presentation/graphTest.js
+++ howrufrontend/src/components/presentation/graphTest.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Line } from "react-chartjs-2";
 import { MDBContainer } from "mdbreact";
 import { Router, Switch, Route } from "react-router-dom";
+import './graph.css';
 
 class ChartsPage extends React.Component {
   state = {
     dataLine: {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      labels: ["1", "2", "3", "4", "5", "6", "7","8","9","10","11","12","13","14","15"],
       datasets: [
         {
           label: "My First dataset",
@@ -24,32 +25,32 @@ class ChartsPage extends React.Component {
           pointHoverRadius: 5,
           pointHoverBackgroundColor: "rgb(0, 0, 0)",
           pointHoverBorderColor: "rgba(220, 220, 220,1)",
-          pointHoverBorderWidth: 2,
+          pointHoverBorderWidth: 1,
           pointRadius: 1,
-          pointHitRadius: 10,
-          data: [65, 59, 80, 81, 56, 55, 40]
+          pointHitRadius: 1,
+          data: [65, 59, 80, 81, 56, 55, 40, 200]
         },
-        {
-          label: "My Second dataset",
-          fill: true,
-          lineTension: 0.3,
-          backgroundColor: "rgba(184, 185, 210, .3)",
-          borderColor: "rgb(35, 26, 136)",
-          borderCapStyle: "butt",
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: "miter",
-          pointBorderColor: "rgb(35, 26, 136)",
-          pointBackgroundColor: "rgb(255, 255, 255)",
-          pointBorderWidth: 10,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgb(0, 0, 0)",
-          pointHoverBorderColor: "rgba(220, 220, 220, 1)",
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: [28, 48, 40, 19, 86, 27, 90]
-        }
+        // {
+        //   label: "My Second dataset",
+        //   fill: true,
+        //   lineTension: 0.3,
+        //   backgroundColor: "rgba(184, 185, 210, .3)",
+        //   borderColor: "rgb(35, 26, 136)",
+        //   borderCapStyle: "butt",
+        //   borderDash: [],
+        //   borderDashOffset: 0.0,
+        //   borderJoinStyle: "miter",
+        //   pointBorderColor: "rgb(35, 26, 136)",
+        //   pointBackgroundColor: "rgb(255, 255, 255)",
+        //   pointBorderWidth: 10,
+        //   pointHoverRadius: 5,
+        //   pointHoverBackgroundColor: "rgb(0, 0, 0)",
+        //   pointHoverBorderColor: "rgba(220, 220, 220, 1)",
+        //   pointHoverBorderWidth: 2,
+        //   pointRadius: 1,
+        //   pointHitRadius: 10,
+        //   data: [28, 48, 40, 19, 86, 27, 90]
+        // }
       ]
     }
   };
@@ -58,7 +59,7 @@ class ChartsPage extends React.Component {
     return (
         <MDBContainer>
             <h3 className="mt-5">Line chart</h3>
-            <Line data={this.state.dataLine} options={{ responsive: true }} />
+            <Line data={this.state.dataLine} options={{ responsive: true}} />
         </MDBContainer>
     );
   }
