023f49ead05131c2119a7765a67f9b0f4488014b
caluha <ludwig.havasi@gmail.com>
2020-04-04 16:46:48 +0200

    Created my own date-parsing-thingy, which seems like a lot, but now it displays dates the way I prefer.

--- howrufrontend/src/components/presentation/graphTest.js
+++ howrufrontend/src/components/presentation/graphTest.js
@@ -67,13 +67,43 @@ class ChartsPage extends React.Component {
 
           for (const i in result) {
             if (dataLine.labels.length < result.length) {
-              dataLine.labels.push(result[i].responseTime);
+              let label = new Date(result[i].responseTime);
+              dataLine.labels.push(`${label.getDate()} ${this.returnMonth(label.getMonth())}`);
             }
           }
         })
       dataLine.datasets.push(dataSet);
     }
     return dataLine;
+  }
+
+  returnMonth(monthNumber) {
+    switch (monthNumber) {
+      case 0:
+        return "Jan";
+      case 1:
+        return "Feb";
+      case 2: 
+        return "Mar";
+      case 3:
+        return "Apr";
+      case 4:
+        return "May";
+      case 5:
+        return "Jun";
+      case 6:
+        return "Jul";
+      case 7:
+        return "Aug";
+      case 8:
+        return "Sep";
+      case 9:
+        return "Oct";
+      case 10:
+        return "Nov";
+      case 11:
+        return "Dec";
+    }
   }
 
   render() {
