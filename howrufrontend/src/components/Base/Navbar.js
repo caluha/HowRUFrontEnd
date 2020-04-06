e9557b044da12a8892b0409a751ff6a2a8c1f308
HernikV <henrik.vendel@outlook.com>
2020-04-06 15:19:51 +0200

    css is getting there

--- howrufrontend/src/components/Base/Navbar.js
+++ howrufrontend/src/components/Base/Navbar.js
@@ -1,20 +1,19 @@
 import React from "react";
 import {Nav, Navbar} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import howru_logo from '../../images/howru_logo.png'
 
 export default function navbar(props) {
   return (
     <Navbar  collapseOnSelect expand="x1" bg="dark" variant="dark">
       <Navbar.Brand href="/">
-        <h5 style={{color:'white'}}>HowRU {props.user} </h5>
+      <img alt="logo" src={howru_logo} style={{ width: "120px" }} />
       </Navbar.Brand>
-     
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/">My Trackers</Nav.Link>
           <Nav.Link href="create">Create new Tracker</Nav.Link>
-          <Nav.Link href="/chart">Graphs</Nav.Link>
           <Nav.Link onClick={props.logout}>Log Out</Nav.Link>
         </Nav>
       </Navbar.Collapse>
