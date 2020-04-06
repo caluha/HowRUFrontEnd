4d0571b3d75f98bcef8b4e990c5c6bed0fe3b6ce
caluha <ludwig.havasi@gmail.com>
2020-04-06 15:58:32 +0200

    Some cleaning up in the graphTest.js. Procrastination is real.

--- howrufrontend/src/components/presentation/graphTest.js
+++ howrufrontend/src/components/presentation/graphTest.js
@@ -15,51 +15,8 @@ class ChartsPage extends React.Component {
       currentQuestion: -1,
       allResponses: {},
       dataLine: {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [
-          {
-            label: "My First dataset",
-            fill: true,
-            lineTension: 0.3,
-            backgroundColor: "rgba(225, 204,230, .3)",
-            borderColor: "rgb(205, 130, 158)",
-            borderCapStyle: "butt",
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: "miter",
-            pointBorderColor: "rgb(205, 130,1 58)",
-            pointBackgroundColor: "rgb(255, 255, 255)",
-            pointBorderWidth: 10,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgb(0, 0, 0)",
-            pointHoverBorderColor: "rgba(220, 220, 220,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: [65, 59, 80, 81, 56, 55, 40]
-          },
-          {
-            label: "My Second dataset",
-            fill: true,
-            lineTension: 0.3,
-            backgroundColor: "rgba(184, 185, 210, .3)",
-            borderColor: "rgb(35, 26, 136)",
-            borderCapStyle: "butt",
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: "miter",
-            pointBorderColor: "rgb(35, 26, 136)",
-            pointBackgroundColor: "rgb(255, 255, 255)",
-            pointBorderWidth: 10,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgb(0, 0, 0)",
-            pointHoverBorderColor: "rgba(220, 220, 220, 1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: [28, 48, 40, 19, 86, 27, 90]
-          }
-        ]
+        labels: [],
+        datasets: []
       },
       showAllResponses:true,
       isFetching: true
@@ -171,9 +128,8 @@ class ChartsPage extends React.Component {
     this.setState({ currentQuestion: event.target.id });
     console.log("Current question: " + curQuestion)
     for (const index in this.state.allResponses.question) {
-      // console.log("DONKEY");
       if (this.state.allResponses.question[index] == curQuestion) {
-        // console.log("I REACHED THIS POINT");
+        console.log(this.state.allResponses.question[index])
 
         let label = this.state.allResponses.labels;
         let newData = this.state.allResponses.datasets[index].data;
@@ -212,8 +168,6 @@ class ChartsPage extends React.Component {
       }
     }
   }
-
-
 
   renderQuestionSelect = () => {
     return this.props.location.state.questions.map((e) =>
