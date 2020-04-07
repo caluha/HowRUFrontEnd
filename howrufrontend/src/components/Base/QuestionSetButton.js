9343845e0cabc3ab91ee024ba28cd1a6728ead23
johanback <johan.g.back@gmail.com>
2020-04-07 20:42:54 +0200

    Calendar icon now green if you've already anwered it today. Some async problems though.

--- howrufrontend/src/components/Base/QuestionSetButton.js
+++ howrufrontend/src/components/Base/QuestionSetButton.js
@@ -2,20 +2,34 @@ import React from 'react';
 import chart from '../../images/chart.png';
 import arrow from '../../images/arrow.png';
 import unanswered_01 from '../../images/unanswered_01.png';
+import answered_01 from '../../images/answered_01.png';
 import { NavLink } from "react-router-dom";
 
 
 class QuestionSetButton extends React.Component {
 
     render() {
-        return (
-            <div className="box" id="lightred_box">
-                <NavLink exact to={"/" + this.props.name} className="pen"><img alt="Go!" src={arrow} style={{ height: "25px" }} /></NavLink>
-                <NavLink exact to="/unchecked" className="pen"><img alt="Unanswered" src={unanswered_01} style={{ height: "25px" }} /></NavLink>
-                <NavLink to={{pathname: "/chart", state: {questions: this.props.questions, name: this.props.name}}} className="pen"><img alt="Data" src={chart} style={{ height: "25px" }} /></NavLink>
-                <div>{this.props.name}</div>
-            </div>
-        );
+        console.log(this.props.answered);
+        if(this.props.answered){
+            return (
+                <div className="box" id="lightred_box">
+                    <NavLink exact to={"/" + this.props.name} className="pen"><img alt="Go!" src={arrow} style={{ height: "25px" }} /></NavLink>
+                    <NavLink exact to="/unchecked" className="pen"><img alt="Answered" src={answered_01} style={{ height: "25px" }} /></NavLink>
+                    <NavLink to={{pathname: "/chart", state: {questions: this.props.questions, name: this.props.name}}} className="pen"><img alt="Data" src={chart} style={{ height: "25px" }} /></NavLink>
+                    <div>{this.props.name}</div>
+                </div>
+            );
+        } else {
+            return (
+                <div className="box" id="lightred_box">
+                    <NavLink exact to={"/" + this.props.name} className="pen"><img alt="Go!" src={arrow} style={{ height: "25px" }} /></NavLink>
+                    <NavLink exact to="/unchecked" className="pen"><img alt="Unanswered" src={unanswered_01} style={{ height: "25px" }} /></NavLink>
+                    <NavLink to={{pathname: "/chart", state: {questions: this.props.questions, name: this.props.name}}} className="pen"><img alt="Data" src={chart} style={{ height: "25px" }} /></NavLink>
+                    <div>{this.props.name}</div>
+                </div>
+            );
+        }
+        
     }
 }
 
