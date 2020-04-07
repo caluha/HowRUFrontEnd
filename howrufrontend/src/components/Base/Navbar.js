90ee40c304e6912bb2ced9f936d284b3b0f83da2
Jacob Winding <jacobkwinding@gmail.com>
2020-04-07 10:57:17 +0200

    Fixed links in Navbar to use NavLink from react-router.

--- howrufrontend/src/components/Base/Navbar.js
+++ howrufrontend/src/components/Base/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Nav, Navbar} from "react-bootstrap";
+import {NavLink} from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import howru_logo from '../../images/howru_logo.png'
 
@@ -12,9 +13,9 @@ export default function navbar(props) {
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="/">My Trackers</Nav.Link>
-          <Nav.Link href="create">Create new Tracker</Nav.Link>
-          <Nav.Link onClick={props.logout} href="/">Log Out</Nav.Link>
+          <NavLink to="/">My Trackers</NavLink>
+          <NavLink to="/create">Create new Tracker</NavLink>
+          <NavLink onClick={props.logout} href="/">Log Out</NavLink>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
